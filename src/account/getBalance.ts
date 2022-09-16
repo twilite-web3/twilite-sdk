888bce807271d0de87bd9d314645c175a471a748
christophertdoc <christophertdoc@gmail.com>
2022-09-16 18:58:50 -0400

    handle getBalance and getBalances errors

--- src/account/getBalance.ts
+++ src/account/getBalance.ts
@@ -5,15 +5,25 @@ const config = require('../../config.json')
 
 module.exports = {
   getBalance: async (params: { apiKey: string; address: string; network: string }) => {
-    const response = await axios.get(`${config.endpoint}account/getBalance`, {
-      headers: {
-        'x-api-key': params.apiKey,
-      },
-      params: {
-        address: params.address,
-        network: params.network
-      }
-    })
-    return response.data
+    
+    try {
+
+      const response = await axios.get(`${config.endpoint}account/getBalance`, {
+        headers: {
+          'x-api-key': params.apiKey,
+        },
+        params: {
+          address: params.address,
+          network: params.network
+        }
+      })
+      return response.data
+
+    } catch(error) {
+
+      throw error;
+
+    }
+
   }
 }
